1e8284beb34425cd077b460fa9a16401e62f1ecc
Andrei Klimkou <47963809+Vokmilk@users.noreply.github.com>
2020-03-23 23:51:31 +0300

    feat: add solution subtask 1

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,9 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+   var arr4=sadArray.filterIndexed { index, i -> index==0 || index==sadArray.size-1|| sadArray[index+1]+sadArray[index-1] > sadArray[index] }.toIntArray()
+        var arr5=arr4.filterIndexed { index, i -> index==0 || index==arr4.size-1|| arr4[index+1]+arr4[index-1] > arr4[index] }.toIntArray()
+        var arr6=arr5.filterIndexed { index, i -> index==0 || index==arr5.size-1|| arr5[index+1]+arr5[index-1] > arr5[index] }.toIntArray()
+return  arr6
     }
 }
