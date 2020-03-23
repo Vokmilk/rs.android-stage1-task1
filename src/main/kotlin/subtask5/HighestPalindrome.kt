745b30f3f3be31895f1f9d80a7d1d6b6e25f7e0e
Andrei Klimkou <47963809+Vokmilk@users.noreply.github.com>
2020-03-23 22:19:42 +0300

    feat: add solution subtask 5

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -1,9 +1,39 @@
 package subtask5
 
 class HighestPalindrome {
-
-    // TODO: Complete the following function
+    var nine  = 57.toChar()
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var count: Int =0
+        var arr = digitString.toMutableList()
+        for (i in 0..n/2-1) {
+            if (digitString[i].toInt()!= digitString[n-1-i].toInt()){
+                count +=1
+            }
+            else {
+            }
+        }
+        if (count > k && k!=count){
+            return "-1"
+        }
+        else if (count==1 ||  k==1){
+            for (i in 0..n/2) {
+                if (digitString[i] == digitString[n-1-i]){
+                }
+                else if (digitString[i] > digitString[n-1-i]){
+                    arr[n-1-i]=arr[i]
+                }
+                else arr[i]=arr[n-1-i]
+            }
+            return arr.joinToString(separator="")
+        }
+        else{
+            arr.set(0,nine)
+            arr.set(arr.lastIndex,nine)
+
+            return  highestValuePalindrome(n, k-2,arr.joinToString(separator=""))
+        }
+
+
     }
+
 }
